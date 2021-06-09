b6f0633c5184c3feb843d7dba428c3b4aa0501b5
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-06-09 12:30:48 +0200

    Displayed amounts and delete button only on hover

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,15 +1,25 @@
 import React from "react";
 
-const Accounts = () => {
+const Accounts = (props) => {
+  const amounts = props.data.map((transaction) => transaction.amount);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0);
+
+  const expense =
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1;
+
   return (
     <div className="income-expense-container">
       <div className="income-container">
         <h4 className="header">Income</h4>
-        <p className="income-expense">+100.00</p>
+        <p className="income-expense">{income}.00</p>
       </div>
       <div className="expense-container">
         <h4 className="header">Expense</h4>
-        <p className="income-expense">-250.00</p>
+        <p className="income-expense">{expense}.00</p>
       </div>
     </div>
   );
